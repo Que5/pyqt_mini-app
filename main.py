33c7b4317c8bb096637c3e736a2ead9364fccd9e
Que5 <phumlanibrightdlamini@gmail.com>
2024-06-06 20:09:49 +0200

    Added functionality

--- main.py
+++ main.py
@@ -1,27 +1,43 @@
-from PyQt6.QtWidgets import QApplication, QVBoxLayout, QLabel, QWidget, QGridLayout, QLineEdit
+from PyQt6.QtWidgets import QApplication, QVBoxLayout, QLabel, QWidget, QGridLayout, QLineEdit, QPushButton
 import sys
+from datetime import datetime
 
 class AgeCalculator(QWidget):
     def __init__(self):
-        grid = QGridLayout()
         super().__init__()
+        grid = QGridLayout()
+        self.setWindowTitle("Age Calculator")
 
         # Create widgets
         name_label = QLabel("Name")
-        name_line_edit = QLineEdit()
+        self.name_line_edit = QLineEdit()
 
-        date_birth_label =QLabel("Date of Birth MM/DD/YY:")
-        date_birth_line_edit = QLineEdit()
+        date_birth_label =QLabel("Date of Birth DD/MM/YY:")
+        self.date_birth_line_edit = QLineEdit()
 
-        calculate_button = QPushButton()
+        calculate_button = QPushButton("Calculate Age")
+        calculate_button.clicked.connect(self.calculate_age)
+        self.output_label = QLabel("")
 
         # Add widgets to grid
         grid.addWidget(name_label, 0, 0)
-        grid.addWidget(name_line_edit, 0, 1)
+        grid.addWidget(self.name_line_edit, 0, 1)
         grid.addWidget(date_birth_label, 1, 0)
-        grid.addWidget(date_birth_line_edit, 1, 1)
+        grid.addWidget(self.date_birth_line_edit, 1, 1)
+        grid.addWidget(calculate_button, 2, 0, 1, 2)
+        grid.addWidget(self.output_label, 3, 0, 1, 2)
+
 
         self.setLayout(grid)
+    
+    def calculate_age(self):
+        current_year = datetime.now().year
+        date_of_birth = self.date_birth_line_edit.text()
+        year_of_birth = datetime.strptime(date_of_birth, "%d/%m/%Y").date().year
+        age = current_year - year_of_birth
+        self.output_label.setText(f"{self.name_line_edit} is {age} years old.")
+
+
 
 app = QApplication(sys.argv)
 age_calculator = AgeCalculator()
